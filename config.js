b0d1916715f211c81e52373c6528c6cdede45ba1
Jacob Hsu <jacobhsu@dopod.com>
2015-09-03 17:35:52 +0800

    https://nodejs.org/api/modules.html#modules_module_exports

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+    return {
+        server: {
+            hostname :'localhost',
+            host:'127.0.0.1',
+            port: 8888
+        },
+    };
+};
---
+++ start.js
@@ -0,0 +1,5 @@
+var config  = require('./config')();
+console.log(config.server);
+
+
+
