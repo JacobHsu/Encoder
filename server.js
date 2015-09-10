fbca2e6b52c5bc6f56673c42f1601cf88470f2b8
Jacob Hsu <jacobhsu@dopod.com>
2015-09-10 17:33:59 +0800

    http://expressjs.com/starter/hello-world.html

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+exports.start = function(config) {
+    var app = express();
+
+    app.get('/', function (req, res) {
+      res.send('Hello World!');
+    });
+
+    var server = app.listen(config.port, function() {
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('[server] Listening at http://%s:%s', host, port); //http://localhost:8888
+    });
+}
--- start.js
+++ start.js
@@ -1,5 +1,8 @@
+var server  = require('./server');
 var config  = require('./config')();
 console.log(config.server);
+
+server.start(config.server);
 
 
 
