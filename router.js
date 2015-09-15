dc53620d738c6cb6cff77100f2a6deade8e88e54
Jacob Hsu <jacobhsu@dopod.com>
2015-09-15 17:34:02 +0800

    http://expressjs.com/api.html#req.body

--- router.js
+++ router.js
@@ -1,4 +1,9 @@
+var bodyParser = require('body-parser');
+
 module.exports = function(app) {
+
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
     app.post('/', function (req, res) {
       res.send('POST request to homepage');
