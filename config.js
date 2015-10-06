981c0ae233f57e988c2c831af1a372e523340b0a
Jacob Hsu <jacobhsu@dopod.com>
2015-10-06 17:33:30 +0800

    https://en.wikipedia.org/wiki/Immediately-invoked_function_expression

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-module.exports = function(app) {
+module.exports = function() {
     return {
         server: {
             hostname :'localhost',
             host:'127.0.0.1',
             port: 8888
+        },
+        database: {
+            mongodb : 'mongodb://localhost:27017/myproject'
         },
     };
 };
--- models/job.js
+++ models/job.js
@@ -1,11 +1,13 @@
 var async = require('async');
 var uuid = require('node-uuid');
+var mongoClient = require('mongodb').MongoClient;
+
+var config  = require('../config')();
 
 //module.exports. As with any variable, if you assign a new value to it, it is no longer bound to the previous value.
 module.exports = function() {
     return new Job();
 };
-
 
 var Job = function () {};
 Job.prototype.push = function(task, myCallback) {
@@ -25,6 +27,7 @@ Job.prototype.push = function(task, myCallback) {
             var fileId;
             fileId = uuid.v4();
             console.log(fileId);
+            console.log(config.database.mongodb);
             callback(null, fileId);
         },
         function(fileId, callback) {
